46ece890f171f0acf31efe5cbc966c13e49ba70c
Akio Miyao <miyao@affrc.go.jp>
2014-08-01 15:48:05 +0900

    Update fetch_fastq.sh
    Version of sratoolkit was changed to 2.3.5-2.

--- read/fetch_fastq.sh
+++ read/fetch_fastq.sh
@@ -4,11 +4,11 @@
 #
 #
 # Download sratoolkit from
-# http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?cmd=show&f=software&m=software&s=software
-# e.g. tar xvfz sratoolkit.2.3.4-2-centos_linux64.tar.gz in your home directory.
+# http://www.ncbi.nlm.nih.gov/Traces/sra/?view=software
+# e.g. tar xvfz sratoolkit.2.3.5-2-centos_linux64.tar.gz in your home directory.
 # 
-~/sratoolkit.2.3.4-2-centos_linux64/bin/fastq-dump --split-files -A SRR823377
-~/sratoolkit.2.3.4-2-centos_linux64/bin/fastq-dump --split-files -A SRR823382
-~/sratoolkit.2.3.4-2-centos_linux64/bin/fastq-dump --split-files -A SRR556173
-~/sratoolkit.2.3.4-2-centos_linux64/bin/fastq-dump --split-files -A SRR556174
-~/sratoolkit.2.3.4-2-centos_linux64/bin/fastq-dump --split-files -A SRR556175
+~/sratoolkit.2.3.5-2-centos_linux64/bin/fastq-dump --split-files -A SRR823377
+~/sratoolkit.2.3.5-2-centos_linux64/bin/fastq-dump --split-files -A SRR823382
+~/sratoolkit.2.3.5-2-centos_linux64/bin/fastq-dump --split-files -A SRR556173
+~/sratoolkit.2.3.5-2-centos_linux64/bin/fastq-dump --split-files -A SRR556174
+~/sratoolkit.2.3.5-2-centos_linux64/bin/fastq-dump --split-files -A SRR556175
