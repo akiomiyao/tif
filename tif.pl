fc5947879ff475a473ad2da241f4bb4acba4b400
Akio Miyao <miyao@affrc.go.jp>
2019-05-27 22:10:22 +0900

    Update tif.pl

--- tif.pl
+++ tif.pl
@@ -9,17 +9,11 @@
 # This code is derived from software contributed to NIAS and NARO by
 # Akio Miyao.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
+# Redistribution and use in source forms, with or without modification,
+# are permitted provided that the following conditions
 # are met:
 # 1. Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the Institute nor the names of its contributors
-#    may be used to endorse or promote products derived from this software
-#    without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -82,8 +76,8 @@ open(IN, $ref);
 while(<IN>){
     chomp;
     if (/^>/){
-	$name = join('', (split)[0, 1]);
-	$name =~ s/^>//;
+        s/^>//;
+        $name = (split)[0];
     }else{
 	y/acgtn/ACGTN/;
 	$chr{$name} .= $_;
